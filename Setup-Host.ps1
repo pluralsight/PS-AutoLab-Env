a9902cf28e2b23bd89a8991408b0acd39137636e
theJasonHelmick <jason-helmick@pluralsight.com>
2016-09-14 13:23:32 -0700

    setup host

---
+++ Setup-Host.ps1
@@ -0,0 +1,30 @@
+﻿# Trying a new direction
+
+Write-Warning "This will reboot your computer"
+Start-Sleep 5
+
+
+# For remoting commands to VM's - have the host set trustedhosts to *
+Write-Output "Setting TrustedHosts to * so that remoting commands to VM's work properly"
+$trust = Get-Item -Path WSMan:\localhost\Client\TrustedHosts 
+If ($trust.value -eq "" -or $trust.value -eq "*"){
+#Why not concatenate?
+    Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value * -Force
+} Else {
+    Write-Output "Your trustedhosts has a value $($trust.Value)"
+
+}
+
+# Lability install
+Get-PackageSource -Name PSGallery | Set-PackageSource -Trusted -Force -ForceBootstrap
+Install-Module -Name Lability
+
+# SEtup host Env.
+Start-LabHostConfiguration -verbose
+
+# Restart-Computer
+Write-Warning @"
+    You must restart this computer
+    From Powershell: Restart-Computer
+"@
+
