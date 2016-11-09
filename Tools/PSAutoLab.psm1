424b246a205352b26bd60ecab97c6b4382f70eb6
Jason Helmick <jason-helmick@pluralsight.com>
2016-11-08 17:56:06 -0700

    Module ready for test

--- Tools/PSAutoLab.psm1
+++ Tools/PSAutoLab.psm1
@@ -21,10 +21,12 @@ demonstrations and would need to be modified for your environment.
 #Requires -version 5.0
 #Requires -runasadministrator
 
+#region Setup-Lab
 Function Setup-Lab {
     Param (
-        $Path = $PSScriptRoot
+        [string]$Path = $PSScriptRoot
     )
+
     Write-Host -ForegroundColor Green -Object @"
 
         This is the Setup-Lab script. This script will perform the following:
@@ -101,3 +103,351 @@ Function Setup-Lab {
 
 
 }
+#endregion Setup-lab
+
+#region Run-Lab
+Function Run-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the Run-Lab script. This script will perform the following:
+    
+        * Start the Lab environment
+    
+        Note! If this is the first time you have run this, it can take up to an hour
+        for the DSC configs to apply. 
+        This only occurs the first time.
+
+"@
+
+    Write-Host -ForegroundColor Cyan -Object 'Starting the lab environment'
+    # Creates the lab environment without making a Hyper-V Snapshot
+    Start-Lab -ConfigurationData .\*.psd1 
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        Next Steps:
+
+        Run the following to validatae when configurations have converged:
+        .\Validate-Lab.ps1
+
+        To enable Internet access for the VM's, run:
+        .\Enable-Internet.ps1
+
+        To stop the lab VM's:
+        .\Shutdown-lab.ps1
+
+        When the configurations have finished, you can checkpoint the VM's with:
+        .\Snapshot-Lab.ps1
+
+        To quickly rebuild the labs from the checkpoint, run:
+        .\Refresh-Lab.ps1
+
+        To destroy the lab to build again:
+        .\Wipe-Lab.ps1   
+
+
+"@
+}
+#endregion setup-lab
+
+#region Enable-Internet
+Function Enable-Internet {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the Enable-Internet script. This script will perform the following:
+    
+        * Enable Internet to the VM's 
+    
+        * Note! - If this generates an error, you are already enabled, or one of the default settings below
+                    does not match your .PSD1 configuration
+   
+        *DevNote! - Currently working on a better solution to pull those values
+
+"@
+
+
+
+        $LabData = Import-PowerShellDataFile -Path .\*.psd1
+        $LabSwitchName = $labdata.NonNodeData.Lability.Network.name 
+        $GatewayIP = $Labdata.AllNodes.DefaultGateway
+        $GatewayPrefix = $Labdata.AllNodes.SubnetMask
+        $NatNetwork = $Labdata.AllNodes.IPnetwork
+        $NatName = $Labdata.AllNodes.IPNatName
+
+
+        $Index = Get-NetAdapter -name "vethernet ($LabSwitchName)" | Select-Object -ExpandProperty InterfaceIndex
+        New-NetIPAddress -InterfaceIndex $Index -IPAddress $GatewayIP -PrefixLength $GatewayPrefix -ErrorAction SilentlyContinue
+        # Creating the NAT on Server 2016 -- maybe not work on 2012R2
+        New-NetNat -Name $NatName -InternalIPInterfaceAddressPrefix $NatNetwork -ErrorAction SilentlyContinue   
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        Next Steps:
+    
+        When complete, run:
+        .\Run-Lab.ps1
+
+        And run:
+        .\Validate-Lab.ps1
+
+"@
+}
+#endregion Enable-Internet
+
+#region Validate-Lab
+Function Validate-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host "[$(Get-Date)] Starting the VM testing process. This could take some time complete. Errors are expected until all tests complete successfully." -ForegroundColor Cyan
+    Start-sleep -Seconds 300
+    $Complete = $False
+
+    do {
+
+    $test= Invoke-Pester -Script $PSScriptRoot\VMValidate.Test.ps1 -quiet -PassThru
+
+    if ($test.Failedcount -eq 0) {
+        $Complete = $True
+    }
+    else {
+        300..1 | foreach {
+        Write-progress -Activity "VM Completion Test" -Status "Tests failed" -CurrentOperation "Waiting until next test run" -SecondsRemaining $_
+        Start-sleep -Seconds 1
+        }
+
+        Write-Progress -Activity "VM Completion Test" -Completed
+    }
+    } until ($Complete)
+
+    #re-run test one more time to show everything that was tested.
+    Invoke-Pester -Script $PSScriptRoot\ValidateVM.Test.ps1
+
+    Write-Host "[$(Get-Date)] VM setup and configuration complete. It is recommended that you snapshot the VMs with Snapshot-Lab.ps1." -ForegroundColor Green
+
+
+}
+#endregion Validate-Lab
+
+#region Shutdown-Lab
+Function Shutdown-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the Shutdown-Lab script. This script will perform the following:
+    
+        * Shutdown the Lab environment:
+
+"@
+
+    Write-Host -ForegroundColor Cyan -Object 'Stopping the lab environment'
+    # Creates the lab environment without making a Hyper-V Snapshot
+    Stop-Lab -ConfigurationData .\*.psd1 
+
+    Write-Host -ForegroundColor Green -Object @"
+
+     Next Steps:
+
+        When the configurations have finished, you can checkpoint the VM's with:
+        .\Snapshot-Lab.ps1
+
+        To quickly rebuild the labs from the checkpoint, run:
+        .\Refresh-Lab.ps1
+
+        To start the lab environment:
+        .\Run-Lab.ps1
+
+        To destroy the lab environment:
+        .\wipe-lab.ps1
+
+"@
+}
+#endregion Shutdown-Lab
+
+#region Snapshot-Lab
+Function Snapshot-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the Snapshot-Lab script. This script will perform the following:
+    
+        * Snapshot the lab environment for easy and fast rebuilding
+    
+        Note! This should be done after the configurations have finished
+
+"@
+
+    Write-Host -ForegroundColor Cyan -Object 'Snapshot the lab environment'
+    # Creates the lab environment without making a Hyper-V Snapshot
+    Stop-Lab -ConfigurationData .\*.psd1 
+    Checkpoint-Lab -ConfigurationData .\*.psd1 -SnapshotName LabConfigured
+
+    Write-Host -ForegroundColor Green -Object @"
+
+       Next Steps:
+
+        To start the lab environment, run:
+        .\Run-Lab.ps1
+
+        To quickly rebuild the labs from the checkpoint, run:
+        .\Refresh-Lab.ps1
+
+        To stop the lab environment, run:
+        .\shutdown-lab.ps1
+
+"@
+}
+#endregion
+
+#region Refresh-Lab
+Function Refresh-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the Refresh-Lab script. This script will perform the following:
+    
+        * Refresh the lab from a previous Snapshot 
+    
+        Note! This can only be done if you created a snapshot!
+        .\Snapshot-lab.ps1
+
+"@
+
+    Write-Host -ForegroundColor Cyan -Object 'Snapshot the lab environment'
+    # Creates the lab environment without making a Hyper-V Snapshot
+    Stop-Lab -ConfigurationData .\*.psd1 
+    Restore-Lab -ConfigurationData .\*.psd1 -SnapshotName LabConfigured -force
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        Next Steps:
+
+        To start the lab environment, run:
+        .\run-lab.ps1
+
+        To stop the lab environment, run:
+        .\shutdown-lab.ps1
+
+        To destroy this lab, run:
+        .\Wipe-Lab.ps1
+
+"@
+}
+#endregion Refresh-Lab
+
+#region Wipe-Lab
+Function Wipe-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        This is the wipe-Lab script. This script will perform the following:
+    
+        * Wipe the lab and VM's from your system 
+
+"@
+
+    Pause
+
+    Write-Host -ForegroundColor Cyan -Object 'Removing the lab environment'
+    # Stop the VM's
+    Stop-Lab -ConfigurationData .\*.psd1
+    # Remove .mof iles 
+    Remove-Item -Path .\*.mof
+    # Delete NAT
+    $LabData = Import-PowerShellDataFile -Path .\*.psd1
+    $NatName = $Labdata.AllNodes.IPNatName
+    Remove-NetNat -Name $NatName 
+    # Delete vM's
+    Remove-LabConfiguration -ConfigurationData .\*.psd1 -RemoveSwitch
+    Remove-Item -Path "$((Get-LabHostDefault).DifferencingVHdPath)\*" -Force
+
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        Next Steps:
+
+        Run the following and follow the onscreen instructions:
+        .\Setup-Lab.ps1
+
+        When complete, run:
+        .\Run-Lab.ps1
+
+        Run the following to validate when configurations have converged:
+        .\Validate-Lab.ps1
+
+        To enable Internet access for the VM's, run:
+        .\Enable-Internet.ps1
+
+        To stop the lab VM's:
+        .\Shutdown-lab.ps1
+
+        When the configurations have finished, you can checkpoint the VM's with:
+        .\Snapshot-Lab.ps1
+
+        To quickly rebuild the labs from the checkpoint, run:
+        .\Refresh-Lab.ps1
+
+        To destroy the lab to build again:
+        .\Wipe-Lab.ps1 
+
+
+"@
+}
+#endregion Wipe-Lab
+
+#region Unattend-Lab
+Function Unattend-Lab {
+    Param (
+        [string]$Path = $PSScriptRoot
+    )
+
+    Write-Host -ForegroundColor Green -Object @"
+
+       This runs Setup-Lab, Run-Lab, and validate-Lab
+    
+"@
+ 
+    Write-Host -ForegroundColor Cyan -Object 'Starting the lab environment'
+
+
+    .\Setup-Lab.ps1
+    .\Run-Lab.ps1
+    .\Enable-Internet.ps1
+    .\Validate-Lab.ps1
+
+    Write-Host -ForegroundColor Green -Object @"
+
+        To stop the lab VM's:
+        .\Shutdown-lab.ps1
+
+        When the configurations have finished, you can checkpoint the VM's with:
+        .\Snapshot-Lab.ps1
+
+        To quickly rebuild the labs from the checkpoint, run:
+        .\Refresh-Lab.ps1
+
+"@
+
+}
+#endregion Unattend-Lab
