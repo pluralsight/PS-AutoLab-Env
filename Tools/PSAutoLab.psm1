75350c2d322a2da52cb6752f3a80333982cdc0c2
theJasonHelmick <jason-helmick@pluralsight.com>
2016-11-08 17:28:23 -0700

    module definition

---
+++ Tools/PSAutoLab.psm1
@@ -0,0 +1,20 @@
+<# Notes:
+
+Authors: Jason Helmick and Melissa (Missy) Janusko
+Additional contributors of note: Jeff Hicks
+
+Note: 
+This module should not be considered to follow best practices. This is a collection of 'Control' scripts,
+that use helping information that does not follow Advanced Function best practices.
+This is intentional for the benefit and ease of use for students less familiar with PowerShell.
+The goal was to make maintainence of the control scripts easier.
+    
+       
+Disclaimer
+
+This example code is provided without copyright and AS IS.  It is free for you to use and modify.
+Note: These demos should not be run as a script. These are the commands that I use in the 
+demonstrations and would need to be modified for your environment.
+
+#> 
+
